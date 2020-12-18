b76972da0b3cf57b27d9de129505be0743c71539
davidestevadiaz <estevadiazdavid@gmail.com>
2020-12-18 11:55:51 +0100

    ejercicios

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,7 +1,7 @@
 var num = [1,2,3,4,5];
 
 function multiplicacion(f){
-  return f<num.length ? num[f]*multiplicacion(f+1);
+  return f<num.length ? num[f]*multiplicacion(f+1) : 1;
 }
 
 console.log(multiplicacion(0));
